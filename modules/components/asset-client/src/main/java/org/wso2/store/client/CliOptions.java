a81da838460171e1ad91856caeded6bd073e9833
manoj <manojg@wwso2.com>
2015-01-02 20:03:37 +0530

    Refactor BAM client and asset upload client

--- modules/components/asset-client/src/main/java/org/wso2/store/client/CliOptions.java
+++ modules/components/asset-client/src/main/java/org/wso2/store/client/CliOptions.java
@@ -1,5 +1,4 @@
 /*
-
 	* Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 	* WSO2 Inc. licenses this file to you under the Apache License,
 	* Version 2.0 (the "License"); you may not use this file except
@@ -12,7 +11,7 @@
 	* KIND, either express or implied. See the License for the
 	* specific language governing permissions and limitations
 	* under the License.
-	*/
+*/
 
 package org.wso2.store.client;
 
